a689bf1deea420147e829c21b52c57d6c2281c96
Slendy <josh@slendy.pw>
2024-04-21 02:13:10 -0500

    Add transition to error alert component

--- src/lib/components/ErrorAlert.svelte
+++ src/lib/components/ErrorAlert.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+
 	export let message: string | null = null;
 </script>
 
 {#if message}
-	<div class="alert alert-danger" role="alert">
+	<div class="alert alert-danger" role="alert" transition:slide>
 		<h4 class="alert-heading">Error</h4>
 		<hr />
 		{message}
