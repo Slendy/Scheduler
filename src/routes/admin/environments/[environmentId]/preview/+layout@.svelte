80d42c3bce83959f42b394b6cac8eb83e302c507
Slendy <josh@slendy.pw>
2024-05-11 18:32:28 -0500

    Make preview mode overlay more responsive

--- src/routes/admin/environments/[environmentId]/preview/+layout@.svelte
+++ src/routes/admin/environments/[environmentId]/preview/+layout@.svelte
@@ -12,7 +12,7 @@
 
 <div class="overlay-container transition">
 	<span class="overlay">You are currently in preview mode</span>
-	<a href="/admin/environments/{data.environment._id}" class="overlay-link">Exit preview mode</a>
+	<a href="/admin/environments/{data.environment._id}" class="overlay-link overlay">Exit preview mode</a>
 </div>
 
 <slot />
@@ -21,14 +21,18 @@
 	.overlay-link {
 		font-family: 'Open Sans', 'Helvetica', serif;
 		color: var(--schedule-text);
-		padding-left: 5rem;
 	}
 	.overlay-container {
 		display: flex;
+		flex-wrap: wrap;
+		text-align: center;
+		column-gap: 5em;
+		row-gap: 1em;
 		position: absolute;
 		z-index: 999;
 		align-items: center;
-		top: 1;
+		justify-content: space-around;
+		top: 0;
 		left: 50%;
 		transform: translate(-50%, 25%);
 		background-color: var(--schedule-accent-color);
@@ -37,9 +41,11 @@
         border: 1px solid var(--schedule-scheduleborder);
 		color: var(--schedule-text-color);
 	}
+
 	.overlay {
-		display: block;
+		display: flex;
 		text-align: center;
 		font-family: 'Open Sans', 'Helvetica', serif;
+		min-width: fit-content;
 	}
 </style>
