f49a0be2110b4ef8bfed23fbfe887217b8cd0782
Slendy <josh@slendy.pw>
2024-06-15 13:05:57 -0500

    Improve reliability of UserSearch positioning

--- src/lib/components/UserSearch.svelte
+++ src/lib/components/UserSearch.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-
 	let inputValue: string;
 	let previousInput: string;
 
@@ -45,41 +43,23 @@
 		selectedUser = user;
 		hideResults = true;
 	}
-
-	onMount(() => {
-		// this looks redundant but ResizeObserver handles most zooming scenarios
-		// and resize handles any edge cases
-		let observer = new ResizeObserver(() => {
-			onResize();
-		});
-		window.addEventListener('resize', onResize);
-		onResize();
-		observer.observe(input);
-
-		return () => {
-			window.removeEventListener('resize', onResize);
-			observer.disconnect();
-		}
-	})
-
-	function onResize(){
-		inputBoundingRect = input.getBoundingClientRect();
-		scrollY = window.scrollY;
-		console.log(inputBoundingRect);
-	}
-
-	let input: any;
-	let inputBoundingRect: any;
-	let scrollY: any;
 </script>
 
 <div>
-	<div bind:this={input}>
+	<div>
 		<input
 			type="text"
 			class="form-control"
 			placeholder="Username"
 			bind:value={inputValue}
+			on:focus={() => {
+				if (!selectedUser) {
+					hideResults = false;
+				}
+			}}
+			on:blur={() => {
+				hideResults = true;
+			}}
 			on:keyup={(e) => {
 				if (inputValue == previousInput) return;
 
@@ -90,25 +70,27 @@
 	</div>
 
 	{#if !hideResults}
-		<div
-			class="position-absolute"
-			style="width: {inputBoundingRect?.width}px; top: {inputBoundingRect?.bottom + (scrollY || 0)}px; left: {inputBoundingRect?.left}px; z-index: 99"
-		>
-			<div class="list-group list-group-item-dark rounded-0 rounded-bottom" style="max-height: 10rem; overflow-y: auto;">
-				{#if !fetchTask && inputValue?.length > 0 && results.length == 0}
-					<li class="list-group-item">No results</li>
-				{:else if fetchTask}
-					<li class="list-group-item">Loading...</li>
-				{:else}
-					{#each results as result}
-						<button
-							class="list-group-item list-group-item-action"
-							on:click={() => selectUser(result)}
-						>
-							{result.username}
-						</button>
-					{/each}
-				{/if}
+		<div class="position-relative">
+			<div class="position-absolute" style="width: 100%">
+				<div
+					class="list-group list-group-item-dark rounded-0 rounded-bottom"
+					style="max-height: 10rem; overflow-y: auto;"
+				>
+					{#if !fetchTask && inputValue?.length > 0 && results.length == 0}
+						<li class="list-group-item">No results</li>
+					{:else if fetchTask}
+						<li class="list-group-item">Loading...</li>
+					{:else}
+						{#each results as result}
+							<button
+								class="list-group-item list-group-item-action"
+								on:mousedown={() => selectUser(result)}
+							>
+								{result.username}
+							</button>
+						{/each}
+					{/if}
+				</div>
 			</div>
 		</div>
 	{/if}
